a4c3d0597cd97b88f80650ea1708b8ca16087862
Deus <kirill2689@gmail.com>
2022-01-09 01:41:10 +0300

    Added user search

--- app.py
+++ app.py
@@ -14,10 +14,34 @@ def index():
     return render_template("index.html", posts=posts)
 
 @app.route("/post/<id>")
-def post_info(id):
+def post_info(id):  # add NO POST exception
     post = posts[int(id) - 1]
     post['views_count'] += 1
     return render_template("post.html", post=post)
+
+@app.route("/search/")
+def search_posts():
+    s = request.args.get("s")
+    # print(s)
+    result = []
+    if s:
+        s = s.lower().strip()
+        for post in posts:
+            if s in post['content'].lower():
+                result.append(post)
+            if len(result) == 10:
+                break
+        print(result)
+    return render_template("search.html", posts=result, posts_number=len(result))
+
+
+@app.route("/users/<user>")
+def search_user(user):
+    print(user)
+    result = [x for x in posts if x['poster_name'] == user]
+    print(result)
+    return render_template("user-feed.html", posts=result, user_name=user)
+
 
 
 if __name__ == '__main__':
