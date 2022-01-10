5b4436fe8acdeb4765a2607f8422c7d110e9b9b8
K <kirill2689@gmail.com>
2022-01-10 21:13:20 +0300

    Added comments for the code, removed test output

--- application.py
+++ application.py
@@ -1,95 +1,99 @@
 from flask import Flask, request, render_template, redirect
-import json, os
 from functions import *
 
 app = Flask(__name__)
 
-posts = get_posts()
-bookmarks = get_bookmarks()
+posts = get_posts()  # Получить все посты и комментарии
+bookmarks = get_bookmarks()  # Получить все закладки
+
 
 @app.route("/")
-def index():
+def index():  # главная страница
     return render_template("index.html", posts=posts, bookmarks_counter=len(bookmarks))
 
-@app.route("/post/<id>")
-def post_info(id):  # add NO POST exception
-    post = posts[int(id) - 1]
-    post['views_count'] += 1
-    return render_template("post.html", post=post)
 
-@app.route("/search/")
+@app.route("/post/<id>")
+def post_info(id):  # вывести информацию о посте
+    for post in posts:  # проверка на существование поста
+        if int(id) == post['pk']:
+            post = posts[int(id) - 1]
+            post['views_count'] += 1
+            return render_template("post.html", post=post)
+    return redirect("/", code=302)  # если поста нет, вернуться на главную страницу
+
+
+@app.route("/search/")  # поиск по последовательности букв
 def search_posts():
-    s = request.args.get("s")
-    # print(s)
+    s = request.args.get("s")  # получаем аргумент
     result = []
-    if s:
-        s = s.lower().strip()
+    if s:  # проверка на пустой аргумент
+        s = s.lower().strip()  # поиск не зависит от регистра
         for post in posts:
             if s in post['content'].lower():
                 result.append(post)
-            if len(result) == 10:
+            if len(result) == 10:  # выводим максимум 10 постов
                 break
     return render_template("search.html", posts=result, posts_number=len(result))
 
 
 @app.route("/users/<user>")
-def search_user(user):
-    result = [x for x in posts if x['poster_name'] == user]
+def search_user(user):  # посты пользователя
+    result = [x for x in posts if x['poster_name'] == user]  # поиск постов
     return render_template("user-feed.html", posts=result, user_name=user)
 
+
 @app.route("/tag/<tag>")
-def search_tags(tag):
-    result = [x for x in posts if '#'+tag in x['tags']]
+def search_tags(tag):  # посты по тэгу
+    result = [x for x in posts if '#' + tag in x['tags']]  # поиск тега среди тегов поста
     return render_template("tag.html", posts=result, tag=tag)
+
 
 @app.route("/bookmark/<postid>")
 def bookmark(postid):
-    # проверяем, есть ли пост в закладках
+    # проверяем, есть ли пост в закладках (сделано, чтобы нажатие на флажок могло и добавлять в закладки,
+    # и удалять из закладок)
     for post in bookmarks:
         if int(postid) == post['pk']:
             return redirect(f"/bookmarks/remove/{postid}")  # нужно добавить пост в закладки
     return redirect(f"/bookmarks/add/{postid}")  # нужно удалить пост из закладок
 
-@app.route("/bookmarks/add/<postid>")
-def bookmark_add(postid):
-    for post in posts:
-        if int(postid) == post['pk']:
-            bookmarks.append(post)
-            add_to_bookmarks(post)
-            print("added ", post)
-            return redirect("/", code=302)
-    return redirect("/", code=302)
 
-@app.route("/bookmarks/remove/<postid>")
+@app.route("/bookmarks/add/<postid>")  # добавляем пост в закладки
+def bookmark_add(postid):
+    for post in posts:  # проверка на существование поста
+        if int(postid) == post['pk']:
+            bookmarks.append(post)  # добавляем пост в список закладок
+            add_to_bookmarks(post)  # записываем пост в файл закладок
+            return redirect("/", code=302)
+    return redirect("/", code=302)  # если пост не существует, возвращаемся на главную страницу
+
+
+@app.route("/bookmarks/remove/<postid>")  # удаляем пост из закладок
 def bookmark_del(postid):
-    print("test ", postid)
-    for i in range(len(bookmarks)):
+    for i in range(len(bookmarks)):  # проверка на существование файла в закладках
         if int(postid) == bookmarks[i]['pk']:
-            print("b ", bookmarks[i]['pk'])
             del bookmarks[i]
             remove_from_bookmarks(postid)
-            # print("removed ", posts[i])
-            print(bookmarks)
             return redirect("/", code=302)
     return redirect("/", code=302)
 
-@app.route("/bookmarks")
+
+@app.route("/bookmarks")  # вывести все закладки
 def bookmarks_view():
     return render_template("bookmarks.html", posts=bookmarks)
 
-@app.route("/comment_add/<postid>", methods=["POST"])
+
+@app.route("/comment_add/<postid>", methods=["POST"])  # добавление комментария к посту
 def comment_add(postid):
-    print("id ", postid)
     for i in range(len(posts)):
         if int(postid) == posts[i]['pk']:
-            # print("adding comment")
-            name = request.form.get("name")
+            name = request.form.get("name")  # получение имени и контента из формы
             content = request.form.get("content")
-            print(name, content)
-            add_comment(postid, name, content)
-            posts[i]['comments'] = refresh_comments(int(postid))
-            posts[i]['comments_counter'] = len(posts[i]['comments'])
+            add_comment(postid, name, content)  # запись комментария в файл
+            posts[i]['comments'] = refresh_comments(int(postid))  # добавление комментария в список комментариев поста
+            posts[i]['comments_counter'] = len(posts[i]['comments'])  # обновление счетчика комментариев
             return redirect(f"/post/{postid}")
+
 
 if __name__ == '__main__':
     app.run()
--- functions.py
+++ functions.py
@@ -4,40 +4,27 @@ import os
 DATA_FOLDER = os.path.dirname(os.path.realpath(__file__)) + "\\data"
 
 
-# DATA_FOLDER = "data"
-
-
-# "data/data.json"
-# print(DATA_FOLDER)
-# os.path.join(UPLOAD_FOLDER, 'filename.png')
 def get_posts():
     """
     Получение списка постов с комментариями
     :return: список словарей постов с комментариями
     """
-    # with open('data\\data.json', 'r', encoding='UTF-8') as f:
-    with open('data/data.json', 'r', encoding='UTF-8') as f:
-    # with open(os.path.join(DATA_FOLDER, 'data.json'), 'r', encoding='UTF-8') as f:
+    # with open('data/data.json', 'r', encoding='UTF-8') as f:  # для heroku
+    with open(os.path.join(DATA_FOLDER, 'data.json'), 'r', encoding='UTF-8') as f:  # для тестирования на машине
         raw_json = f.read()
-        # print(raw_json)
     posts = json.loads(raw_json)
     comments = get_comments()
     for post in posts:
-        post['comments'] = [x for x in comments if x['post_id'] == post['pk']]
+        post['comments'] = [x for x in comments if
+                            x['post_id'] == post['pk']]  # добавление комментариев напрямую в пост
         post['comments_counter'] = len(post['comments'])
-        post['conjugate'] = conjugate_comment(post['comments_counter'])
-        # print(post['content'])
-        post['content_short'] = post['content'][:47].strip("., !?") + "..."
-        post['tags'] = get_tags(post)
+        post['conjugate'] = conjugate_comment(post['comments_counter'])  # правильная форма слова "комментарий"
+        post['content_short'] = post['content'][:47].strip("., !?") + "..."  # короткий текст, оканчивается на "..."
+        post['tags'] = get_tags(post)  # получение тегов поста
         for tag in post['tags']:
+            # обрамление тегов в ссылки в полном и кратком содержании
             post['content'] = post['content'].replace(" " + tag + " ", " " + tag_to_link(tag) + " ")
-            # print(post['content'])
             post['content_short'] = post['content_short'].replace(" " + tag + " ", " " + tag_to_link(tag) + " ")
-        # print(post['content'])
-
-        # post['content_very_short'] = post['content'][:7].strip("., !?") + "..."
-    #     print(post)
-    # print(posts)
     return posts
 
 
@@ -46,11 +33,9 @@ def get_comments():
     Получение количества комментариев
     :return: кол-во комментариев
     """
-    # with open('data\\comments.json', 'r', encoding='UTF-8') as f:
-    with open('data/comments.json', 'r', encoding='UTF-8') as f:
-    # with open(os.path.join(DATA_FOLDER, 'comments.json'), 'r', encoding='UTF-8') as f:
+    # with open('data/comments.json', 'r', encoding='UTF-8') as f:  # для heroku
+    with open(os.path.join(DATA_FOLDER, 'comments.json'), 'r', encoding='UTF-8') as f:  # для тестирования на машине
         raw_json = f.read()
-        # print(raw_json)
     comments = json.loads(raw_json)
     return comments
 
@@ -61,7 +46,7 @@ def conjugate_comment(num):
     :param num: количество комментариев
     :return: склонение слова "комментарий"
     """
-    if num % 10 == 1:
+    if num % 10 == 1 or (12 <= num % 100 <= 19):
         return "комментарий"
     if 2 <= num % 10 <= 4:
         return "комментария"
@@ -69,76 +54,102 @@ def conjugate_comment(num):
 
 
 def get_tags(post):
+    """
+    получение всех тегов в посте
+    :param post: пост, в тексте которого ищем теги
+    :return: сортированный список тегов поста без повторений
+    """
     tags = [x for x in post['content'].split(" ") if x.startswith("#")]
-    # print(tags)
     return sorted(list(set(tags)))
 
 
 def tag_to_link(tag):
+    """
+    Обрамление тега в ссылку для шаблона
+    :param tag: тег
+    :return: строка, которая станет ссылкой в шаблоне
+    """
     return f'<a href="/tag/{tag[1:]}">{tag}</a>'
 
+
 def get_bookmarks():
-    with open('data/bookmarks.json', 'r', encoding='UTF-8') as f:
-    # with open(os.path.join(DATA_FOLDER, 'bookmarks.json'), 'r', encoding='UTF-8') as f:
+    """
+    получение постов в закладках
+    :return: список постов, помещенных в закладки
+    """
+    # with open('data/bookmarks.json', 'r', encoding='UTF-8') as f:  # для heroku
+    with open(os.path.join(DATA_FOLDER, 'bookmarks.json'), 'r', encoding='UTF-8') as f:  # для тестирования на машине
         raw_json = f.read()
     posts = json.loads(raw_json)
     return posts
 
+
 def add_to_bookmarks(post):
-    with open('data/bookmarks.json', 'r', encoding='UTF-8') as f:
-    # with open(os.path.join(DATA_FOLDER, 'bookmarks.json'), 'r', encoding='UTF-8') as f:
+    """
+    запись поста в файл закладок
+    :param post: пост, который нужно добавить в закладки
+    """
+    # with open('data/bookmarks.json', 'r', encoding='UTF-8') as f:  # для heroku
+    with open(os.path.join(DATA_FOLDER, 'bookmarks.json'), 'r', encoding='UTF-8') as f:  # для тестирования на машине
         raw_json = f.read()
-        # print(raw_json)
     posts = json.loads(raw_json)
     posts.append(post)
 
     raw_json = json.dumps(posts)
-    with open('data/bookmarks.json', 'w', encoding='UTF-8') as f:
-    # with open(os.path.join(DATA_FOLDER, 'bookmarks.json'), 'w', encoding='UTF-8') as f:
+    # with open('data/bookmarks.json', 'w', encoding='UTF-8') as f:  # для heroku
+    with open(os.path.join(DATA_FOLDER, 'bookmarks.json'), 'w', encoding='UTF-8') as f:  # для тестирования на машине
         f.write(raw_json)
 
 
 def remove_from_bookmarks(postid):
-    with open('data/bookmarks.json', 'r', encoding='UTF-8') as f:
-    # with open(os.path.join(DATA_FOLDER, 'bookmarks.json'), 'r', encoding='UTF-8') as f:
+    """
+    удаление поста из файла закладок
+    :param postid: id поста, который нужно удалить
+    """
+    # with open('data/bookmarks.json', 'r', encoding='UTF-8') as f:  # для heroku
+    with open(os.path.join(DATA_FOLDER, 'bookmarks.json'), 'r', encoding='UTF-8') as f:  # для тестирования на машине
         raw_json = f.read()
-        # print(raw_json)
     posts = json.loads(raw_json)
-    # posts.append(post)
-    # posts = [x for x in posts if not (post['pk'] == x['pk'])]
-    print(postid)
     for i in range(len(posts)):
-        # print(posts[i])
         if int(postid) == int(posts[i]['pk']):
-            print("testing")
             del posts[i]
             break
 
     raw_json = json.dumps(posts)
-    with open('data/bookmarks.json', 'w', encoding='UTF-8') as f:
-    # with open(os.path.join(DATA_FOLDER, 'bookmarks.json'), 'w', encoding='UTF-8') as f:
+    # with open('data/bookmarks.json', 'w', encoding='UTF-8') as f:  # для heroku
+    with open(os.path.join(DATA_FOLDER, 'bookmarks.json'), 'w', encoding='UTF-8') as f:  # для тестирования на машине
         f.write(raw_json)
 
 
 def add_comment(postid, name, content):
+    """
+    добавление комментария к посту
+    :param postid: id поста, в который записываем комментарий
+    :param name: имя пользователя, оставившего комментарий
+    :param content: содержание поста
+    :return:
+    """
     comments = get_comments()
-    comment_id = max([i['pk'] for i in comments])
-    print(comment_id)
-    comment = {
+    comment_id = max([i['pk'] for i in comments])  # получение нового id комментария (максимальный id из доступных + 1)
+    comment = {  # создание нового комментария
         'post_id': int(postid),
         'commenter_name': name,
         'comment': content,
         'pk': comment_id + 1,
     }
-    comments.append(comment)
+    comments.append(comment)  # запись комментария
 
     raw_json = json.dumps(comments)
-    with open('data/comments.json', 'w', encoding='UTF-8') as f:
-    # with open(os.path.join(DATA_FOLDER, 'comments.json'), 'w', encoding='UTF-8') as f:
+    # with open('data/comments.json', 'w', encoding='UTF-8') as f:  # для heroku
+    with open(os.path.join(DATA_FOLDER, 'comments.json'), 'w', encoding='UTF-8') as f:  # для тестирования на машине
         f.write(raw_json)
 
+
 def refresh_comments(postid):
+    """
+    обновление комментариев у поста
+    :param postid: id поста
+    :return: новый список комментариев к посту
+    """
     comments = get_comments()
     return [x for x in comments if x['post_id'] == postid]
-
-# get_posts()
